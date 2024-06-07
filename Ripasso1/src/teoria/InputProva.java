6abfe0e15fa10beb23f1f69e8bee25c5d4344594
a.ingaramo <a.ingaramo@page93.it>
2024-06-07 13:29:34 +0200

    update

--- Ripasso1/src/teoria/InputProva.java
+++ Ripasso1/src/teoria/InputProva.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class InputProva {
 	public static void main(String[] args) {
 		Scanner mioScanner = new Scanner(System.in); //input da tastiera
-		System.out.println("Scrivi il tuo nome");
-		String nome = mioScanner.nextLine();
+//		System.out.println("Scrivi il tuo nome");
+//		String nome = mioScanner.nextLine();
+		String nome = leggiNome(mioScanner);
 		System.out.println("Il tuo nome è: " + nome);
+		
 		System.out.println("Scrivi la tua età");
 		int eta = mioScanner.nextInt();
 		System.out.println("la tua età è: " + eta);
 		mioScanner.close();
+		
 	}
+
+	private static String leggiNome(Scanner mioScanner) {
+		System.out.println("Scrivi il tuo nome");
+		String nome = mioScanner.nextLine();
+		return nome;
+	}
+	
+	
 }
